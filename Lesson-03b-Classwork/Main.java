8cf39dc7df07a14a23eaa5b447e23ae4c7fbe36a
KAIZHILOH1 <kaizhil@nycstudents.net>
2025-09-25 13:27:58 +0000

    update

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Kai Zhi Loh");
+   System.out.println("15-84 Bell Blvd");
+   System.out.println("Kai Zhi Loh \n15-84 Bell Blvd");
+    
+  }
+
+  
+}
---
+++ Lesson-07-activity/Main.java
@@ -0,0 +1,106 @@
+
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+/*  
+    Challenge 1:
+    1) Create the variables, ask the user for the variable values, write the equation in file EQ1-act6 and display the equation value.
+*/
+  System.out.println("Enter x");
+  double x = Input.readDouble();
+  double y = 0;
+  double result = Math.pow("x,7")
+
+/*  
+    Challenge 2:
+    1) Create the variables, ask the user for the variable values, write the equation in fileEQ1.1-act6 and display the equation value.
+*/
+  System.out.println("Enter z");
+  double z = Input.readDouble();
+  double result = Math.pow("z,3")+ 5;
+
+/*  
+    Challenge 3:
+    Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
+    
+
+
+
+*/
+System.out.println("Enter t and r")
+double t = Input.readDouble();
+double r = Input.readDouble();
+double s = 0;
+double result = Math.pow(t,5)* Math.pow((r+2),4);
+
+
+/*  
+    Challenge 4:
+    Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
+    
+*/
+
+System.out.println("Enter A and B")
+double A = Input.readDouble();
+double B = Input.readDouble();
+double C = 0;
+double result = Math.sqrt(A+B);
+
+/*  
+    Challenge 5:
+    Create the variables, ask the user for the variable values, write the equation in file EQ4-act6 and display the equation value..
+    
+*/
+
+System.out.println("Enter x1 and x2 and y2 and y1")
+double x1 = Input.readDouble();
+double x2 = Input.readDouble();
+double y1 = Input.readDouble();
+double y2 = Input.readDouble();
+double d = 0;
+double result = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+
+
+/*  
+    Challenge 6:
+    Create the variables, ask the user for the variable values, write the equation g=sin(deg) and display the equation value..
+    
+*/
+
+System.out.println("Enter deg");
+double deg = Input.readDouble();
+double g = 0;
+double result = Math.sin*(deg);
+
+/*  
+    Challenge 7:
+    Create the variables, ask the user for the variable values, write the equation in file EQ5-act6 and display the equation value.
+    
+*/
+
+System.out.println("Enter m and n")
+double x1 = Input.readDouble();
+double x2 = Input.readDouble();
+double k = 0;
+double result = Math.pow(m,5)/Math.sqrt(n+1);
+
+
+/*  
+    *** Bonus Challenge ***:
+    Create the variables, ask the user for the variable values, write the equation in file Ch-act6 and display the equation value.
+
+    HINT: What does the "plus minus: after "-b" mean.
+*/
+
+
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
